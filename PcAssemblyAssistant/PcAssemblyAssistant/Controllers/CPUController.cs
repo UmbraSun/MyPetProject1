9da57a0874a4dcefd2c34e3cb763fa08a481da04
UmbraSun <kirill.xodyukov@gmail.com>
2022-11-21 16:05:39 +0600

    fail

---
+++ PcAssemblyAssistant/PcAssemblyAssistant/Controllers/CPUController.cs
@@ -0,0 +1,44 @@
+﻿using BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Models.API_models.CPUmodelAPI;
+using PcAssemblyAssistant.AppHelper;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace PcAssemblyAssistant.Controllers
+{
+    /// <summary>
+    /// CPU controller
+    /// </summary>
+    [Route("api/CPU")]
+    [ApiController]
+    [ModelValidation]
+    public class CPUController : ControllerBase
+    {
+        private readonly ICPUService _service;
+
+        /// <summary>
+        /// constructor with DI
+        /// </summary>
+        /// <param name="service">CPU service</param>
+        public CPUController(ICPUService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// Add new CPU 
+        /// </summary>
+        /// <param name="model">CPU model with characteristics</param>
+        /// <returns></returns>
+        [Route("Add")]
+        [HttpPost]
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest)]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized)]
+        public async Task Add(AddCPUModel model)
+        {
+            await _service.BaseCreateModelAsync(model);
+        }
+    }
+}
